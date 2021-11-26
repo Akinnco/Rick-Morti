b93c58cbeee4d22cd4707cf9a4bcfe857e95afe4
Akinnco <69485226+Akinnco@users.noreply.github.com>
2021-11-26 16:35:04 +0300

    İlk yüklemeler ekranların olusturulması

---
+++ src/screens/DetailScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const DetailScreen = () => {
+  return (
+    <View>
+      <Text>DetailScreen</Text>
+    </View>
+  );
+};
+
+export default DetailScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/ListScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const ListScreen = () => {
+  return (
+    <View>
+      <Text>ListScreen</Text>
+    </View>
+  );
+};
+
+export default ListScreen;
+
+const styles = StyleSheet.create({});
